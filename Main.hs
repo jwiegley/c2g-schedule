2673f17f60c1f02a74e5567a12a34c8d387f0f75
John Wiegley <johnw@newartisans.com>
2023-04-27 17:45:04 -0700

    Initial version

---
+++ Main.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternGuards #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+module Main where
+
+import Data.SBV
+
+-- | A solution is a sequence of group assignments for each participant
+type Solution = [SWord8]
+
+isValid :: [Group] -> [Participant] -> Solution -> SBool
+isValid _ _ _ = undefined
+
+scheduleGroups :: [Group] -> [Participant] -> IO ()
+scheduleGroups _ _ = undefined
+
+-- Given a CSV file of the proper schedule, generate and display an updated
+-- version of that CSV file which assigns participants to groups.
+c2gSchedule :: FilePath -> IO ()
+c2gSchedule _ = undefined
